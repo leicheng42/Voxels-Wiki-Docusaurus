a1e968cbd84ae360f0f5f6bdbb3172bcd0fa9bc3
leicheng42 <leicheng42@gmail.com>
2023-09-16 15:12:27 +0800

    add translate

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -147,6 +147,36 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'RL2LQ0OV78',
+  
+        // Public API key: it is safe to commit it
+        apiKey: '852dd3a638f16237b6a042a39106ad13',
+  
+        indexName: 'voxels-leicheng42',
+  
+        // Optional: see doc section below
+        contextualSearch: true,
+  
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: 'external\\.com|domain\\.com',
+  
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        // replaceSearchResultPathname: {
+        //   from: '/docs/', // or as RegExp: /\/docs\//
+        //   to: '/',
+        // },
+  
+        // Optional: Algolia search parameters
+        searchParameters: {},
+  
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+  
+        //... other Algolia params
+        insights: true, // Optional, automatically send insights when user interacts with search results
+      },
     }),
 };
 
